3205f90c07e5614ad90cea59eb59b7bc883167df
Ben Greenman <benjaminlgreenman@gmail.com>
2020-02-26 21:24:37 -0500

    ** try to fix deps for pkg-build

--- redex-abbrevs-doc/info.rkt
+++ redex-abbrevs-doc/info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection 'multi)
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-doc" "redex-doc" "redex-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-doc" "redex-doc" "redex-lib" "redex-abbrevs-lib"))
 (define pkg-desc "documentation of \"redex-abbrevs\"")
